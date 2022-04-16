1fa36b17589b3a56f6caed11f7200f3f3c2fb0d7
csachin42 <csachin42@gmail.com>
2022-04-16 15:51:09 +0530

    Primittive

---
+++ src/first/Primittive.java
@@ -0,0 +1,17 @@
+package first;
+
+public class Primittive {
+    public static void main (String[] args){
+        int a = 102;
+        float b = 15.6f;
+        char c = 'C';
+        boolean d = true;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+
+
+
+    }
+}
